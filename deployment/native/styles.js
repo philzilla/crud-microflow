0bcc88cb0d999bf0ef3d904a12eef9fcc1101d37
philzilla <bonjour@philippeyim.fr>
2021-11-30 14:08:04 +0100

    Initial commit

---
+++ deployment/native/styles.js
@@ -0,0 +1,10 @@
+import * as style0 from "C:/Users/Serveur/Documents/Mendix/Libraire-main/themesource/anychart_buildingblocks/native/main";
+import * as style1 from "C:/Users/Serveur/Documents/Mendix/Libraire-main/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/Serveur/Documents/Mendix/Libraire-main/themesource/webactions/native/main";
+import * as style3 from "C:/Users/Serveur/Documents/Mendix/Libraire-main/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/Serveur/Documents/Mendix/Libraire-main/themesource/librairie/native/main";
+import * as style5 from "C:/Users/Serveur/Documents/Mendix/Libraire-main/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5]);
